f26d0e565e8f1b8a4368a38b52e25af80d542e06
Murilo Dutra <murilodut@gmail.com>
2019-11-11 10:58:25 -0300

    Update App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const App = () => <div>App</div>;
+import PullRequestList from './PullRequestList';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/:user/:repo" component={PullRequestList} />
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
