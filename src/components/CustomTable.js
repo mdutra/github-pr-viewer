e06a3b5976b4863b59497dda25c627dac340f978
Murilo Dutra <murilodut@gmail.com>
2019-11-11 09:03:44 -0300

    Customize table pagination

--- src/components/CustomTable.js
+++ src/components/CustomTable.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MaterialTable from 'material-table';
 
-function CustomTable({ repo, data }) {
+function CustomTable({ repo, data, pageSize, onChangePage }) {
   const fromRepo = repo ? ` from ${repo}` : '';
 
   return (
@@ -13,7 +13,16 @@ function CustomTable({ repo, data }) {
       options={{
         search: false,
         header: false,
+        pageSize,
+        pageSizeOptions: [],
+        showFirstLastPageButtons: false,
       }}
+      localization={{
+        pagination: {
+          labelDisplayedRows: '{from}--{to}',
+        },
+      }}
+      onChangePage={onChangePage}
     />
   );
 }
@@ -23,10 +32,14 @@ CustomTable.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({ title: PropTypes.string.isRequired }),
   ),
+  pageSize: PropTypes.number,
+  onChangePage: PropTypes.func,
 };
 
 CustomTable.defaultProps = {
   repo: undefined,
+  pageSize: 5,
+  onChangePage: undefined,
 };
 
 export default CustomTable;
